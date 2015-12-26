bb30a1512b8694170186a8c8bb5b7e3203c1be19
Rohan <tidyway.in@gmail.com>
2015-12-26 15:26:02 +0530

    [MIGRATED]web_environment_ribbon: migrated to v9

--- web_environment_ribbon/__openerp__.py
+++ web_environment_ribbon/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': "Web Environment Ribbon",
-    'version': '8.0.0.1.0',
+    'version': '9.0.0.1.0',
     'category': 'Web',
     'author': 'Francesco OpenCode Apruzzese,Odoo Community Association (OCA)',
     'website': 'https://it.linkedin.com/in/francescoapruzzese',
